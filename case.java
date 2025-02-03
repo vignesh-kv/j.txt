8ad7188b9442b7c02a6ce62b64bf0975314e7491
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-03 19:33:13 +0530

    Add files via upload
    without break  statement

---
+++ case.java
@@ -0,0 +1,18 @@
+class cas{
+public static void main(String[]args){
+String s1="A";
+switch(s1)
+{
+case "A":
+System.out.println("addition");
+case "B":
+System.out.println("subtraction");
+case "c":
+System.out.println("multiplication");
+case "d":
+System.out.println("division");
+default:
+System.out.println("default");
+}
+}
+}
