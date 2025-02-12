b741175ebc3c216abf66b3f1dd4698beaf41ca34
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-12 22:06:27 +0530

    Add files via upload

---
+++ ini.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class ini{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int i=15;
+int j=115;
+if(j%5==0 && i%5==0){
+System.out.print("hero is hero");
+}else{
+System.out.print("willain is willain");
+}
+}
+}
