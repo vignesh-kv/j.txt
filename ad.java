8edc02fdb1d956bf84a219bfef2d5fbeea4a1e3d
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-16 20:56:15 +0530

    Add files via upload
    addition of three numbers

---
+++ ad.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ad{
+public static void main(String[]arg){
+System.out.println("Enter the values:");
+Scanner ve=new Scanner(System.in);
+int a=ve.nextInt();
+int b=ve.nextInt();
+int c=ve.nextInt();
+int d=a+b+c;
+System.out.print(d);
+}
+}
